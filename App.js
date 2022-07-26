b51590316e5d9ece8d4d9ad29936961b8582e4c9
U210709027 <ardacan271@gmail.com>
2022-07-26 20:46:27 +0300

    .

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  StatusBar,
+  View,
   StyleSheet,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,6 +10,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import Anasayfa from './screens/Anasayfa';
 import Sepetim from './screens/Sepetim';
 import Favorilerim from './screens/Favorilerim';
+import Kategoriler from './screens/Kategoriler';
 
 const Tab = createBottomTabNavigator();
 
@@ -27,15 +28,18 @@ const App = () => {
               iconName = focused ? "cart" : "cart-outline";
             } else if (route.name === "Favorilerim") {
               iconName = focused ? "heart" : "heart-outline"
+            } else if (route.name === "Kategoriler") {
+              iconName = focused ? "grid" : "grid-outline"
             }
 
             return <Icon name={iconName} size={size} color={color}/>
           },
-          tabBarActiveTintColor: "#00d6e6",
+          tabBarActiveTintColor: "#00c9d4",
           tabBarInactiveTintColor: "gray",
         })}>
-        <Tab.Screen name="Anasayfa" component={Anasayfa}/>
-        <Tab.Screen name="Sepetim" component={Sepetim}/>
+        <Tab.Screen name="Anasayfa" component={Anasayfa} options={{header: () => (<View/>)}}/>
+        <Tab.Screen name='Kategoriler' component={Kategoriler}/>
+        <Tab.Screen name="Sepetim" component={Sepetim} />
         <Tab.Screen name='Favorilerim' component={Favorilerim}/>
       </Tab.Navigator>
     </NavigationContainer>  
--- screens/Anasayfa.js
+++ screens/Anasayfa.js
@@ -4,10 +4,37 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useEffect, useState } from 'react';
 
-const Anasayfa = () => {
+const Anasayfa = ({navigation}) => {
+
+  useEffect(() => {
+    navigation.setOptions({
+      header: () => (
+        <View style={{
+          height: 55,
+          width: "100%",
+          backgroundColor: "white",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          }}>
+          <Text style={{
+            marginLeft: 20,
+            fontSize: 24,
+            color: "#00c9d4",
+          }}>hepsiorada</Text>
+          <TouchableOpacity>
+            <Icon name='person-circle-outline' size={34} color={"black"}/>
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Anasayfa</Text>
